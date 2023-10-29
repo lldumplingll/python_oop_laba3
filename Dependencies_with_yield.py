80c555c5856944946d8d7fb726ba2362b1593cd7
lldumplingll <mariaignascheva@gmail.com>
2023-10-29 21:36:49 +0300

    python_oop_laba3
    python_oop_laba3

---
+++ Dependencies_with_yield.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+from fastapi import Depends
+
+async def dependency_a():
+    dep_a = generate_dep_a()
+    try:
+        yield dep_a
+    finally:
+        dep_a.close()
+
+async def dependency_b(dep_a: Annotated[DepA, Depends(dependency_a)]):
+    dep_b = generate_dep_b()
+    try:
+        yield dep_b
+    finally:
+        dep_b.close(dep_a)
+
+async def dependency_c(dep_b: Annotated[DepB, Depends(dependency_b)]):
+    dep_c = generate_dep_c()
+    try:
+        yield dep_c
+    finally:
+        dep_c.close(dep_b)
+
+class MySuperContextManager:
+    def __init__(self):
+        self.db = DBSession()
+
+    def __enter__(self):
+        return self.db
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.db.close()
+
+async def get_db():
+    with MySuperContextManager() as db:
+        yield db
